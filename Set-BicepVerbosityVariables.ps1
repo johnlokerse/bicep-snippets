f530bb39f77f2bba44870f9ad734ca4dae043798
John <J.G.Lokerse@gmail.com>
2024-10-14 13:22:36 +0200

    Create Set-BicepVerbosityVariables.ps1

---
+++ Set-BicepVerbosityVariables.ps1
@@ -0,0 +1,7 @@
+# For Windows
+$env:BICEP_TRACING_VERBOSITY = $true
+$env:BICEP_TRACING_ENABLED = $true
+
+# For MacOS / Linux
+export BICEP_TRACING_VERBOSITY=$true
+export BICEP_TRACING_ENABLED=$true
